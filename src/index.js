9fb3902ead4169a81324ba7e80b767d0407399a2
Nathnael Desta <destanathnaelataro@gmail.com>
2024-02-13 16:22:52 +0300

    complete basic dropdown

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
+import { forEach } from 'lodash';
 import './style.css';
+
+const close = document.querySelector('.close');
+const basic = document.querySelector('.basic');
+const mobile_menu = document.querySelector('.mobile_menu');
+const dropdowns  = document.querySelectorAll('.dropdown_v');
+const names = document.querySelectorAll(".name");
+
+mobile_menu.addEventListener('click', () => {
+    close.classList.toggle('hidden');
+    basic.classList.toggle('hidden');
+    let parent = mobile_menu.parentElement;
+    let vertical = parent.nextElementSibling;
+    vertical.classList.toggle('none')
+})
+
+dropdowns.forEach((dropdown) => {
+    dropdown.addEventListener('click',() => {
+
+    })
+})
+
+names.forEach((name) => {
+    name.addEventListener('click', () => {
+        let holder = name.nextElementSibling;
+        holder.classList.toggle('none');
+
+    })
+})
+
